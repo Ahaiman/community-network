022a4802c899b985d84ae088f1a9c67bd9521ef2
Avital Haiman <57088761+Ahaiman@users.noreply.github.com>
2020-08-14 16:33:22 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -8,36 +8,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <time.h>
-#include <math.h>
-//#include "./structures/graph.h"
+#include "./structures/graph.h"
 #include "./functions/CreateGraph.c"
 #include <string.h>
 
-
-
-/*SELF NOTE - do not use assert, for any error use "return" something rather then 0*/
 
 int main(int argc, char* argv[]){
 
 	/*Variables deceleration*/
 	graph *input_graph;
 	clock_t start, end;
-	double epsilon = 0.00001;
-	int i = 0;
-//	double *initialVec, *matrixRow, *result, *tmp;
 
 	/*Time measurement */
 	srand(time(NULL));
 	start = clock();
 	(void)argc;
 
-	//input_graph = createGraph(NAME OF FILE);
+	/*Create Graph */
+	input_graph = createGraph(argv[1]);
 
+
+	/*Send to Algorithm 3 */
 
 	end = clock();
-	printf("Prog  took: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+	//printf("Prog  took: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
 	return 0;
 }
 
